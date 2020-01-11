b9f5f86883f6baa06b2d5358b434cf73c7981b0f
Caesar Ladion <caesar.ladion@gmail.com>
2020-01-10 16:10:37 -0800

    update proto file, started CreateTeam() repository.go

--- pkg/service/v1/repository.go
+++ pkg/service/v1/repository.go
@@ -8,11 +8,13 @@ import (
 )
 
 type repository interface {
-  CreateTeam(*v1.Team) (string, error)
-  DeleteTeam(string) (int64, error)
-  AddMember(string, string) (string, error)
-  RemoveMember(string, string) (int64, error)
-  UpsertProject(string, *v1.Project) (int64, error)
+  CreateTeam(context.Context, *v1.Team) (string, error)
+  DeleteTeam(context.Context, string) (int64, error)
+  GetTeamByTeamId(context.Context, string) (*v1.Team, error)
+  GetTeamsByUserId(context.Context, string) ([]v1.Team, error)
+  AddMember(context.Context, string, string) (string, error)
+  RemoveMember(context.Context, string, string) (int64, error)
+  UpsertProject(context.Context, string, *v1.Project) (int64, error)
 }
 
 type teamRepository struct {
@@ -31,4 +33,21 @@ func (r *teamRepository) connect(ctx context.Context) (*sql.Conn, error) {
     return nil, status.Error(codes.Unknown, "failed to connect to database-> "+err.Error())
   }
   return c, nil
+}
+
+func (r *teamRepository) CreateTeam(ctx context.Context, team *v1.Team) (string, error) {
+  // prepare sql statements for teams, skills, members
+  stmt := `INSERT INTO teams (leader, name, open_roles, size, last_active)
+  VALUES(?, ?, ?, ?, ?))`
+
+  // start transaction
+
+  // insert team into teams table capturing the id
+
+  // insert members into members table including team_id field
+
+  // insert skills into skills table including team_id field
+
+  result, err := r.db.Exec(stmt, team.Leader)
+
 }
