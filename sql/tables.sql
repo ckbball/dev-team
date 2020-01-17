8a7b2d22d7215b755b8c735d371c694a732db597
Caesar Ladion <caesar.ladion@gmail.com>
2020-01-16 16:35:18 -0800

    added sql tables

---
+++ sql/tables.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE teams (
+    id int not null PRIMARY key auto_increment,
+    leader varchar(255) not null,
+    team_name varchar(25) not null,
+    open_roles int not null,
+    size int not null,
+    last_active int
+);
+
+CREATE TABLE members (
+    id int not null PRIMARY key auto_increment,
+    user_id int not null,
+    member_email varchar(255) not null,
+    member_role varchar(40) not null,
+    team_id int,
+    FOREIGN KEY(team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE skills (
+    id int not null PRIMARY key auto_increment,
+    skill_name varchar(100) not null,
+    team_id int,
+    FOREIGN KEY(team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE projects (
+    id int not null PRIMARY key auto_increment,
+    goal varchar(1200) not null,
+    project_name varchar(30) not null,
+    github_link varchar(255) not null,
+    complexity int not null,
+    duration int not null,
+    team_id int,
+    FOREIGN KEY(team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE languages (
+    id int not null PRIMARY key auto_increment,
+    lang_name varchar(100) not null,
+    team_id int,
+    FOREIGN KEY(team_id) REFERENCES teams(id)
+);
